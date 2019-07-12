22f9cb0e970262fdf7924a5617488448e4ad29f7
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-07-12 11:24:57 -0400

    Added Flex End and Flex Center

--- script/index.js
+++ script/index.js
@@ -6,3 +6,16 @@ const flexStartDiv = document.querySelector('[ data-flex-start-div ]');
 flexStart.addEventListener('click', () => {
     flexStartDiv.classList.toggle('hidden')
 });
+
+const flexEnd = document.querySelector('[ data-flex-end ]');
+const flexEndDiv = document.querySelector('[ data-flex-end-div ]');
+
+flexEnd.addEventListener('click', () => {
+    flexEndDiv.classList.toggle('hidden')
+});
+const center = document.querySelector('[ data-center ]');
+const centerDiv = document.querySelector('[ data-center-div ]');
+
+center.addEventListener('click', () => {
+    centerDiv.classList.toggle('hidden')
+});
