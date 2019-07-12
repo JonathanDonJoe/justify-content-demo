0473bb147621ca7a498f799dd023e6f4b4b87eae
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-07-12 10:53:52 -0400

    Added user interaction interface

---
+++ script/index.js
@@ -0,0 +1,8 @@
+//jS
+
+const flexStart = document.querySelector('[ data-flex-start ]');
+const flexStartDiv = document.querySelector('[ data-flex-start-div ]');
+
+flexStart.addEventListener('click', () => {
+    flexStartDiv.classList.toggle('hidden')
+});
